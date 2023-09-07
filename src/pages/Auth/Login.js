c6cd21c0a4de746bb8a6509016dd1c629106e1e7
Aslem TURKI <a.turki@sofrecom-tunisie.com>
2023-09-07 18:47:33 +0100

    [v0002] login form setStates.

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./auth.css"
 
 const Login = () => {
+
+    //const [login, setLogin] = useState();
+    //const [password, setPassword] = useState();
+    const [credentials, setCredentials] = useState({
+        login:'',
+        password:''
+    });
+
+    const onChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = (e) => {
+      e.preventDefault();
+    }
+
+
+
+
     return (
         <div className="login_form">
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="group">
                     <label htmlFor="login">Identifiant</label>
-                    <input type="text" name="login"/>
+                    <input type="text" name="login" value={credentials.login} onChange={onChange}/>
                 </div>
                 <div className="group">
                     <label htmlFor="password">Mot de passe</label>
-                    <input type="text" name="password"/>
+                    <input type="text" name="password" value={credentials.password} onChange={onChange}/>
                 </div>
                 <div className="group">
                     <button>Connexion</button>
